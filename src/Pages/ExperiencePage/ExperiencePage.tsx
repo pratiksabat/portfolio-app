666413c6957d33737eb5d83a7f95cda034e10ea3
Praik Sabata <pratik6009sabata@gmail.com>
2022-05-23 09:50:51 +0530

    Experience added

--- src/Pages/ExperiencePage/ExperiencePage.tsx
+++ src/Pages/ExperiencePage/ExperiencePage.tsx
@@ -1,9 +1,31 @@
 import React, { FC } from 'react';
 import './ExperiencePage.scss';
+import data from '../../data/expereince.json'
 
 const ExperiencePage = () => {
   return (
-    <div className='ExperiencePage'>ExperiencePage</div>
+    <div className='ExperiencePage'>
+      <h1>Experience</h1>
+      {
+        data.map((expereince) => {
+          return <div className="exp-card">
+            <div className="work-info">
+                <h4>{expereince.postition}</h4>
+                <p>{expereince.mode}</p>
+                <p>{expereince.timeline}</p>
+                <p>{expereince.type}</p>
+            </div>
+            <div className="about-work">
+                <h4>{expereince.company}</h4>
+                {
+                  expereince.work.map(work => <p> - {work}</p>)
+                }
+            </div>
+          </div>
+        })
+      }
+
+    </div>
   )
 }
 
