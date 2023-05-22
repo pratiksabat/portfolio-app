3e1890eb4a438724ff62f38eba8ac93f359436f8
Pratik Sabata <pratik6009sabata@gmail.com>
2023-05-22 15:43:23 +0530

    Revamped the style & added Animation for Award Component.

--- src/Components/AwardCard/AwardCard.tsx
+++ src/Components/AwardCard/AwardCard.tsx
@@ -1,23 +1,27 @@
 import './AwardCard.scss';
+import { motion } from 'framer-motion';
 // import abc from "../../assets/logo1.png"
 const HiDLogo = require('../../assets/hashedin-logo.png')
 const BsGLogo = require('../../assets/bsg-logo.png')
 
 const AwardCard = (props) => {
   return (
-    <div className='AwardCard'>
-      <div className="award-icon">
+    <motion.div className='AwardCard' whileHover={ {
+      scale: 1.2,
+      transition: { duration: 1 },
+}}>
+      <motion.div className="award-icon">
       <img src={HiDLogo} alt="" height="50px" width="50px" style={{padding:'3px'}} />
-      </div>
-      <div className="award-info">
-        <div className="name">
+      </motion.div>
+      <motion.div className="award-info">
+        <motion.div className="name">
           <h3>{props.data.award}</h3>
-        </div>
-        <div className="place">
+        </motion.div>
+        <motion.div className="place">
           <p>{props.data.company}</p>
-        </div>
-      </div>
-    </div>
+        </motion.div>
+      </motion.div>
+    </motion.div>
   )
 }
 
